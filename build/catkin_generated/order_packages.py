025766973346053ba0737d1f6ea2dcd6bc0d55ab
Jay Francis <jay@reactivetechnologies.com>
2017-09-10 10:52:41 -0400

    First commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/euclid/GitHub/ev3Base_pkg/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/intel/euclid/euclid_ws/devel;/opt/ros/kinetic".split(';') if "/intel/euclid/euclid_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/euclid/GitHub/ev3Base_pkg/devel:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/euclid/bin:/home/euclid/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/euclid/GitHub/ev3Base_pkg/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/euclid/GitHub/ev3Base_pkg/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/euclid/GitHub/ev3Base_pkg/src:$ROS_PACKAGE_PATH"
