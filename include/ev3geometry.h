c01ecb55fa6b7d292d823977c315bc5a0bb0859c
Jay Francis <Jay@ReactiveTechnologies.com>
2017-09-11 11:58:18 -0400

    New robot geometry

--- include/ev3geometry.h
+++ include/ev3geometry.h
@@ -3,8 +3,8 @@
 
 #include<math.h>
 
-const double WheelSpacingM = 87.5 / 1000.0;
-const double WheelDiameterM = 67.0 / 1000.0;
+const double WheelSpacingM = 120.0 / 1000.0;
+const double WheelDiameterM = 94.2 / 1000.0;
 const double WheelCircumferenceM = M_PI * WheelDiameterM;
 
 const int CountsPerRotation = 360;
