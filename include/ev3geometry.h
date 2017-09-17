5c8d9025ea29bb6758407d4d177a2fc1155bf51d
Jay Francis <Jay@ReactiveTechnologies.com>
2017-09-17 10:35:10 -0400

    Changed wheels

--- include/ev3geometry.h
+++ include/ev3geometry.h
@@ -3,8 +3,8 @@
 
 #include<math.h>
 
-const double WheelSpacingM = 120.0 / 1000.0;
-const double WheelDiameterM = 94.2 / 1000.0;
+const double WheelSpacingM = 127.0 / 1000.0;
+const double WheelDiameterM = 68.8 / 1000.0;
 const double WheelCircumferenceM = M_PI * WheelDiameterM;
 
 const int CountsPerRotation = 360;
